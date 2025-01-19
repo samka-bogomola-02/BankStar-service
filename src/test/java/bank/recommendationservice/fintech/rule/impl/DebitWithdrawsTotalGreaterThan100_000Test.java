ffbd6cdd824584e963542491a1bb8cca432086a1
Rafnes <rafzograf@gmail.com>
2025-01-20 00:05:28 +0300

    Юнит-тесты имплементаций интерфейса Rule

---
+++ src/test/java/bank/recommendationservice/fintech/rule/impl/DebitWithdrawsTotalGreaterThan100_000Test.java
@@ -0,0 +1,67 @@
+package bank.recommendationservice.fintech.rule.impl;
+
+import bank.recommendationservice.fintech.exception.NoTransactionsFoundException;
+import bank.recommendationservice.fintech.repository.RecommendationsRepository;
+import bank.recommendationservice.fintech.rule.ProductType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.UUID;
+
+import static bank.recommendationservice.fintech.testdata.RuleImplTestData.TOTAL_HIGH;
+import static bank.recommendationservice.fintech.testdata.RuleImplTestData.TOTAL_LOW;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class DebitWithdrawsTotalGreaterThan100_000Test {
+    @Mock
+    RecommendationsRepository recommendationsRepository;
+
+    @InjectMocks
+    DebitWithdrawsTotalGreaterThan100_000 out;
+    private final UUID userId = UUID.randomUUID();
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("Возвращает true, когда сумма трат по DEBIT больше 100 000")
+    void testEvaluatePositive_1() {
+        when(recommendationsRepository.getWithdrawsOfTypeTotal(userId, ProductType.DEBIT.name())).thenReturn(TOTAL_HIGH);
+
+        //test
+        boolean actual = out.evaluate(userId);
+
+        //check
+        assertTrue(actual);
+        verify(recommendationsRepository).getWithdrawsOfTypeTotal(userId, ProductType.DEBIT.name());
+    }
+
+    @Test
+    @DisplayName("Возвращает false, когда сумма трат по DEBIT меньше 100 000")
+    void testEvaluatePositive_2() {
+        when(recommendationsRepository.getWithdrawsOfTypeTotal(userId, ProductType.DEBIT.name())).thenReturn(TOTAL_LOW);
+
+        //test
+        boolean actual = out.evaluate(userId);
+
+        //check
+        assertFalse(actual);
+        verify(recommendationsRepository).getWithdrawsOfTypeTotal(userId, ProductType.DEBIT.name());
+    }
+
+    @Test
+    @DisplayName("Тест на выброс Exception, если getWithdrawsOfTypeTotal() вернет null")
+    void evaluateTestNegative() {
+        //test and check
+        when(recommendationsRepository.getWithdrawsOfTypeTotal(userId, ProductType.DEBIT.name())).thenReturn(null);
+        assertThrows(NoTransactionsFoundException.class, () -> out.evaluate(userId));
+    }
+}
---
+++ src/test/java/bank/recommendationservice/fintech/testdata/RuleImplTestData.java
@@ -0,0 +1,8 @@
+package bank.recommendationservice.fintech.testdata;
+
+import bank.recommendationservice.fintech.rule.ProductType;
+
+public class RuleImplTestData {
+    public static final Integer TOTAL_HIGH = 120_000;
+    public static final Integer TOTAL_LOW = 10_000;
+}
