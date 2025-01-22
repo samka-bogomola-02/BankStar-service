1cce8e8aafd5d3a8b77a22edf7ecb4082771a8e8
gface <89259112970@mail.ru>
2025-01-22 19:37:49 +0300

    DataSource fixed

--- src/main/java/bank/recommendationservice/fintech/configuration/DataSourceConfiguration.java
+++ src/main/java/bank/recommendationservice/fintech/configuration/DataSourceConfiguration.java
@@ -3,8 +3,10 @@ package bank.recommendationservice.fintech.configuration;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
@@ -16,9 +18,14 @@ public class DataSourceConfiguration {
             @Value("${application.fintech_service-db.url}") String serviceRecommendationsUrl) {
         var dataSource = new HikariDataSource();
         dataSource.setJdbcUrl(serviceRecommendationsUrl);
-        dataSource.setDriverClassName("org.h2.Driver");
         dataSource.setReadOnly(true);
         return dataSource;
+    }
+
+    @Primary
+    @Bean(name = "defaultDataSource")
+    public DataSource defaultDataSource(DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder().build();
     }
 
     @Bean(name = "recommendationsJdbcTemplate")
