7efae52d9b97187935e0dc78a81657513704ed6c
gface <89259112970@mail.ru>
2025-01-26 20:19:06 +0300

    Добавлены методы getAll и delete в сервис и контроллер

--- src/main/java/bank/recommendationservice/fintech/controller/RecommendationDynamicRuleController.java
+++ src/main/java/bank/recommendationservice/fintech/controller/RecommendationDynamicRuleController.java
@@ -7,25 +7,34 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/rule")
 public class RecommendationDynamicRuleController {
 
-        @Autowired
-        private RecommendationDynamicRuleService recommendationDynamicRuleService;
+    @Autowired
+    private RecommendationDynamicRuleService recommendationDynamicRuleService;
 
-        @PostMapping
-        public ResponseEntity<DynamicRule> createRule(@RequestBody DynamicRule rule) {
-                Logger logger = LoggerFactory.getLogger(RecommendationDynamicRuleService.class);
-                logger.info("Добавление нового rule: {}", rule.toString());
+    @PostMapping
+    public ResponseEntity<DynamicRule> createRule(@RequestBody DynamicRule rule) {
+        Logger logger = LoggerFactory.getLogger(RecommendationDynamicRuleService.class);
+        logger.info("Добавление нового rule: {}", rule.toString());
 
-                DynamicRule savedRule = recommendationDynamicRuleService.addRule(rule);
+        DynamicRule savedRule = recommendationDynamicRuleService.addRule(rule);
 
-                return ResponseEntity.status(HttpStatus.CREATED).body(savedRule);
-        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedRule);
+    }
+
+    @DeleteMapping
+    public DynamicRule deleteRule(@RequestBody Long id) {
+        return recommendationDynamicRuleService.deleteDynamicRule(id);
+    }
+
+    @GetMapping
+    public List<DynamicRule> getAllRules() {
+        return recommendationDynamicRuleService.getAllDynamicRules();
+    }
 }
---
+++ src/main/java/bank/recommendationservice/fintech/other/QueryType.java
@@ -0,0 +1,8 @@
+package bank.recommendationservice.fintech.other;
+
+public enum QueryType {
+    USER_OF,
+    ACTIVE_USER_OF,
+    TRANSACTION_SUM_COMPARE,
+    TRANSACTION_SUM_COMPARE_DEPOSIT_WITHDRAW
+}
