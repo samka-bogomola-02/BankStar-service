4890443e2f7968a9de8896c9bb3835169079a109
gface <89259112970@mail.ru>
2025-01-28 20:06:21 +0300

    Исправлен метода addRule + JavaDoc's Все работает

--- src/main/java/bank/recommendationservice/fintech/service/RecommendationDynamicRuleService.java
+++ src/main/java/bank/recommendationservice/fintech/service/RecommendationDynamicRuleService.java
@@ -3,50 +3,55 @@ package bank.recommendationservice.fintech.service;
 import bank.recommendationservice.fintech.exception.RulesNotFoundException;
 import bank.recommendationservice.fintech.interfaces.DynamicRuleRepository;
 import bank.recommendationservice.fintech.model.DynamicRule;
-import bank.recommendationservice.fintech.model.DynamicRuleQuery;
-import bank.recommendationservice.fintech.model.DynamicRuleQueryArgument;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
+//@RequiredArgsConstructor
+@Slf4j
 public class RecommendationDynamicRuleService {
     @Autowired
-    private DynamicRuleRepository dynamicRuleRepository;
+    private final DynamicRuleRepository dynamicRuleRepository;
+
+    public RecommendationDynamicRuleService(DynamicRuleRepository dynamicRuleRepository) {
+        this.dynamicRuleRepository = dynamicRuleRepository;
+    }
+
     Logger logger = LoggerFactory.getLogger(RecommendationDynamicRuleService.class);
 
+
+    /**
+     * Сохраняет новое правило в базе данных.
+     * <p>
+     * Перед сохранением правила, оно обеспечивает, что запросы правила и их аргументы должным образом связаны с
+     * правилом.
+     * <p>
+     * Данный метод является транзакционным, что означает, что если возникнет любая ошибка во время операции
+     * сохранения, операция будет отменена.
+     *
+     * @param rule правило, которое нужно сохранить
+     * @return сохраненное правило
+     * @throws RulesNotFoundException если правило не может быть сохранено
+     */
 
     @Transactional
     public DynamicRule addRule(DynamicRule rule) {
         logger.info("Добавление нового правила: {}", rule.toString());
-        List<DynamicRuleQuery> queries = rule.getQueries();
-        List<DynamicRuleQuery> updatedQueries = new ArrayList<>(); // Новый список для обновленных запросов
 
-        if (queries != null) {
-            for (DynamicRuleQuery query : queries) {
-                List<DynamicRuleQueryArgument> arguments = query.getArguments();
-                List<DynamicRuleQueryArgument> updatedArguments = new ArrayList<>();
-
-                if (arguments != null) {
-                    updatedArguments.addAll(arguments);
+        if (rule.getQueries() != null) {
+            rule.getQueries().forEach(query -> {
+                if (query.getArguments() != null) {
+                    query.getArguments().forEach(argument -> argument.setDynamicRuleQuery(query));
                 }
-                // Добавление нового аргумента
-                updatedArguments.add(new DynamicRuleQueryArgument());
-                // Устанавливаем обновленный список аргументов
-                query.setArguments(updatedArguments);
-                // Устанавливаем связь с правилом
                 query.setDynamicRule(rule);
-                // Добавляем обновленный запрос в новый список
-                updatedQueries.add(query);
-            }
+            });
         }
-        // После завершения итерации устанавливаем обновленные запросы к правилу
-        rule.setQueries(updatedQueries);
         return dynamicRuleRepository.save(rule);
     }
 
