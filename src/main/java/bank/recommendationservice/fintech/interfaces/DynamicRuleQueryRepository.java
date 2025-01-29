6afd02b20b441e7b21eb1941a55c580eaa797099
gface <89259112970@mail.ru>
2025-01-29 17:53:28 +0300

    Fixed delete method in  RecommendationDynamicRuleService

--- src/main/java/bank/recommendationservice/fintech/interfaces/DynamicRuleQueryRepository.java
+++ src/main/java/bank/recommendationservice/fintech/interfaces/DynamicRuleQueryRepository.java
@@ -2,9 +2,11 @@ package bank.recommendationservice.fintech.interfaces;
 
 import bank.recommendationservice.fintech.model.DynamicRuleQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface DynamicRuleQueryRepository extends JpaRepository<DynamicRuleQuery, Long> {
     List<DynamicRuleQuery> findByDynamicRuleId(Long ruleId);
 }
