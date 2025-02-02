0a100f6850ba4b5eeea525a16ba2562cb8bfee52
Rafnes <rafzograf@gmail.com>
2025-02-02 22:24:04 +0300

    связь ruleStats изменена на OneToOne + небольшой фикс swagger config

--- src/main/java/bank/recommendationservice/fintech/configuration/SwaggerConfig.java
+++ src/main/java/bank/recommendationservice/fintech/configuration/SwaggerConfig.java
@@ -3,19 +3,25 @@ package bank.recommendationservice.fintech.configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.tags.Tag;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${build.version}")
+    private String appVersion;
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Fintech API")
-                        .version("1.0")
-                        .description("API для управления новым сервисом Fintech - Recommended Program for SkyPro School"))
+                        .title(appName)
+                        .version(appVersion)
+                        .description("API для управления новым сервисом Fintech - Recommendation Service for SkyPro School"))
                 .addTagsItem(new Tag().name("Health Check").description("Эндпоинты для проверки работоспособности приложения"))
                 .addTagsItem(new Tag().name("Recommendation").description("Эндпоинты для управления рекомендациями"))
                 .addTagsItem(new Tag().name("Dynamic Rule").description("Эндпоинты для управления динамическими правилами"));
