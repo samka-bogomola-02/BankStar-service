64ac705ba464d7809f76c8daa45284125dcb77b8
gface <89259112970@mail.ru>
2025-02-06 00:49:29 +0300

    Добавлен менеджмент

---
+++ src/main/java/bank/recommendationservice/fintech/controller/InfoController.java
@@ -0,0 +1,25 @@
+package bank.recommendationservice.fintech.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/management")
+public class InfoController {
+    private final BuildProperties buildProperties;
+
+    @Autowired
+    public InfoController(BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
+    }
+
+    @GetMapping("/info")
+    public String info() {
+        return "name = " + buildProperties.getName() + ", version = " + buildProperties.getVersion();
+    }
+}
+
